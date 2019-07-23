f059f27620e6289516d990c3b9da009bfb1572b2
Kalim <kalim.r1989@gmail.com>
2019-07-23 14:39:45 +0200

    add featureZ

--- App.java
+++ App.java
@@ -4,8 +4,12 @@ public class App {
         System.out.println("App.main");
     }
 
-    private void doStuff(){
+    private static void doStuff(){
         System.out.println("FeatureX: Do stuff!!");
+    }
+
+    private static void featureZ(){
+        doStuff();
     }
 }
 
